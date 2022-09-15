119d8a3ad2e9bb80bd0bd51b03dc64efc8aec752
CuamatziCRC <cuamatzicrc@gmail.com>
2022-09-15 16:51:42 -0500

    Ejercicio en JavaScript
    Manejo de un arreglo

---
+++ ArregloEnJavaScript.js
@@ -0,0 +1,12 @@
+"use strict"
+
+var animales = ["Perros","Gatos","Conejos"];
+
+var totalAnimales = animales.length;
+//Una variable para medir la longitud del arreglo
+//Para no preguntar constantemente en cada interacion del for
+for(var k = 0, k = totalAnimales ; k < k++){
+	console.log(animales[k]);
+}
+//Muestra los  elementos del arreglo
+
